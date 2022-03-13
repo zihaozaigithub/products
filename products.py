b0b2f2dd8bc64d5421b6bc56d7688604b6b241e1
Leo Li <Lizihaomail@gmail.com>
2022-03-13 14:27:53 -0700

    product price list 2

--- products.py
+++ products.py
@@ -9,3 +9,8 @@ print(products)
 
 for p in products:
 	print(p[0], 'price is: ', p[1])
+
+with open ('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品' + ',' + '价格' + '\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
